83eef3fe3fdc7be36d698e999cab5fc2d8a47384
Romik Sargsyan <sargsyanromik6@gmail.com>
2021-04-01 13:20:22 +0300

    first commit

---
+++ resources/views/emails/SendEmail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Amerio Grupė</title>
+</head>
+<body>
+asdfghjhgfds
+</body>
+</html>
